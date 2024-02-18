89cf5281f88908a94194822cf8f8d15e6631f23d
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-02-18 15:06:58 +0900

    initial set

--- pretotype2/script.js
+++ pretotype2/script.js
@@ -1,17 +1,18 @@
 function searchInfluencer() {
     // Get the influencer username from the input field
-    var influencerUsername = document.getElementById('influencerInput').value;
+    var influencerUsername1 = document.getElementById('influencerInput1').value;
+    var influencerUsername2 = document.getElementById('influencerInput2').value;
 
     // Check if the input is not empty
-    if (influencerUsername.trim() !== '') {
+    if (influencerUsername1.trim() !== '' && influencerUsername2.trim() !== ''){
         // Create the URL for the new page, for example: analyze.html?q=username
-        var url = 'analyze.html?q=' + encodeURIComponent(influencerUsername);
+        var url = 'analyze.html?q=' + encodeURIComponent(influencerUsername2);
 
         // Redirect to the new page
         window.location.href = url;
     } else {
         // Show an alert or handle the case where the input is empty
-        alert('워너비 계정을 입력해주세요.');
+        alert('계정이 비어있습니다. \n 본인의 계정과 워너비 인플루언서의 계정 모두 입력해주세요.');
     }
 }
 
