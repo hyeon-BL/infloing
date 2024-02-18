47712628ba53a6a86e647f67f113fda3d163bc3d
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-02-18 15:13:46 +0900

    pretotype2_modified from pretotype1

---
+++ pretotype2/target_save_influencer.php
@@ -0,0 +1,20 @@
+<?php
+$file = 'target_influencer_list.txt';
+
+if (isset($_POST['influencerInput'])) {
+    $influencerInput = $_POST['influencerInput'];
+
+    if (filesize($file) == 0) {
+        file_put_contents($file, $influencerInput);
+    } else {
+        $file2 = file_get_contents($file) . "\n" . $influencerInput;
+        file_put_contents($file, $file2);
+    }
+
+    header('Location: analyze.html');
+    exit();
+} else {
+    // influencerInput이 설정되지 않은 경우 처리
+    echo '유효하지 않은 입력입니다.';
+}
+?>
